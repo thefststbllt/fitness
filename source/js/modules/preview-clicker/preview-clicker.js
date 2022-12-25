b1526024fbcfac9b4a352cbed12116897c90d822
thefststbllt <bel12pasha@yandex.ru>
2022-12-25 12:19:35 +0300

    fix: video focus fix

--- source/js/modules/preview-clicker/preview-clicker.js
+++ source/js/modules/preview-clicker/preview-clicker.js
@@ -1,11 +1,13 @@
 export const initVideoPreview = () => {
   const videoContainer = document.querySelector('.block-about__video-container');
+  const pageVideo = document.querySelector('.block-about__video');
   const videoButton = document.querySelector('.block-about__video-button');
 
   const addPreview = () => {
-    if (!videoContainer && !videoButton) {
+    if (!videoContainer && !videoButton && !pageVideo) {
       return;
     }
+    pageVideo.style.display = ('none');
     videoContainer.classList.remove('block-about__video-container--opened');
   };
 
@@ -14,6 +16,7 @@ export const initVideoPreview = () => {
       return;
     }
     videoContainer.classList.add('block-about__video-container--opened');
+    pageVideo.style.display = ('block');
   };
   addPreview();
   if (videoButton) {
